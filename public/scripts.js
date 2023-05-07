cf9d69ff7cf348e36fb6e5e2e9753c311e777909
Luis Sanchez <89441865+SiulandCheese@users.noreply.github.com>
2023-05-07 18:21:23 -0400

    404 errors

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,234 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+var rps = false; 
+var opponent = false;
+
+function toggleRPS() { 
+    rps = !rps
+    var toggle = document.getElementsByClassName("Toggle")[0]; 
+
+    if (rps) { toggle.style.display = 'inline';}
+    else { toggle.style.display = 'none';}
+    
+
+}
+
+function toggleOpponent() {
+     opponent = !opponent 
+    
+}
+
+async function playRock() { 
+    var player; 
+    var opponentText; 
+    var shot = "rock"; 
+    if (rps && opponent) {
+        // Play RPS against Opponent
+        const response = await fetch(`/app/rps/play/${shot}`);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.result.toUpperCase();
+        player.innerHTML = "You: " + jsonData.player;
+        opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+        resultPopup.style.display = 'inline';
+    }
+    else if (rps) { 
+        // Play RPS without Opponent 
+        const response = await fetch("/app/rps");
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.player.toUpperCase();
+        popup.style.display = 'inline';
+    }
+    else if (opponent) { 
+        // Play an opponent in RPSLS
+        const response = await fetch(`/app/rpsls/play/${shot}`);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.result.toUpperCase();
+        player.innerHTML = "You: " + jsonData.player;
+        opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+        popup.style.display = 'inline';
+    }
+    else { 
+        // Play yourself in RPSLS
+        const response = await fetch("./app/rpsls");
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.player.toUpperCase();
+        popup.style.display = 'inline';
+    }
+}
+
+async function playPaper() { 
+    var player; 
+    var opponentText; 
+    var shot = "paper"; 
+    if (rps && opponent) {
+        // Play RPS against Opponent
+        const response = await fetch(`/app/rps/play/${shot}`);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.result.toUpperCase();
+        player.innerHTML = "You: " + jsonData.player;
+        opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+        resultPopup.style.display = 'inline';
+    }
+    else if (rps) { 
+        // Play RPS without Opponent 
+        const response = await fetch("./app/rps");
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.player.toUpperCase();
+        popup.style.display = 'inline';
+    }
+    else if (opponent) { 
+        // Play an opponent in RPSLS
+        const response = await fetch(`/app/rpsls/play/${shot}`);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.result.toUpperCase();
+        player.innerHTML = "You: " + jsonData.player;
+        opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+        popup.style.display = 'inline';
+    }
+    else { 
+        // Play yourself in RPSLS
+        const response = await fetch("./app/rpsls");
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.player.toUpperCase();
+        popup.style.display = 'inline';
+    }
+}
+
+async function playScissors() { 
+    var player; 
+    var opponentText; 
+    var shot = "scissors"; 
+    if (rps && opponent) {
+        // Play RPS against Opponent
+        const response = await fetch(`/app/rps/play/${shot}`);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.result.toUpperCase();
+        player.innerHTML = "You: " + jsonData.player;
+        opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+        resultPopup.style.display = 'inline';
+    }
+    else if (rps) { 
+        // Play RPS without Opponent 
+        const response = await fetch("./app/rps");
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.player.toUpperCase();
+        popup.style.display = 'inline';
+    }
+    else if (opponent) { 
+        // Play an opponent in RPSLS
+        const response = await fetch(`/app/rpsls/play/${shot}`);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.result.toUpperCase();
+        player.innerHTML = "You: " + jsonData.player;
+        opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+        popup.style.display = 'inline';
+    }
+    else { 
+        // Play yourself in RPSLS
+        const response = await fetch("./app/rpsls");
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.player.toUpperCase();
+        popup.style.display = 'inline';
+    }
+}
+
+async function playLizard() { 
+    var player; 
+    var opponentText; 
+    var shot = "lizard"; 
+    if (rps && opponent) {
+        // Play RPS against Opponent
+        const response = await fetch(`/app/rps/play/${shot}`);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.result.toUpperCase();
+        player.innerHTML = "You: " + jsonData.player;
+        opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+        resultPopup.style.display = 'inline';
+    }
+    else if (rps) { 
+        // Play RPS without Opponent 
+        const response = await fetch("./app/rps");
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.player.toUpperCase();
+        popup.style.display = 'inline';
+    }
+    else if (opponent) { 
+        // Play an opponent in RPSLS
+        const response = await fetch(`/app/rpsls/play/${shot}`);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.result.toUpperCase();
+        player.innerHTML = "You: " + jsonData.player;
+        opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+        popup.style.display = 'inline';
+    }
+    else { 
+        // Play yourself in RPSLS
+        const response = await fetch("./app/rpsls");
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.player.toUpperCase();
+        popup.style.display = 'inline';
+    }
+}
+
+async function playSpock() { 
+    var player; 
+    var opponentText; 
+    var shot = "spock"; 
+    if (rps && opponent) {
+        // Play RPS against Opponent
+        const response = await fetch(`/app/rps/play/${shot}`);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.result.toUpperCase();
+        player.innerHTML = "You: " + jsonData.player;
+        opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+        resultPopup.style.display = 'inline';
+    }
+    else if (rps) { 
+        // Play RPS without Opponent 
+        const response = await fetch("./app/rps");
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.player.toUpperCase();
+        popup.style.display = 'inline';
+    }
+    else if (opponent) { 
+        // Play an opponent in RPSLS
+        const response = await fetch(`/app/rpsls/play/${shot}`);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.result.toUpperCase();
+        player.innerHTML = "You: " + jsonData.player;
+        opponentText.innerHTML = "Opponent: " + jsonData.opponent;
+        popup.style.display = 'inline';
+    }
+    else { 
+        // Play yourself in RPSLS
+        const response = await fetch("./app/rpsls");
+        const jsonData = await response.json();
+        console.log(jsonData);
+        result.innerHTML = jsonData.player.toUpperCase();
+        popup.style.display = 'inline';
+    }
+}
+
+function reset() { location.reload; }
+
+
--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 //// Load most basic dependencies
 // Create require function 
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
-import { createRequire } from 'node:module';
+import { createRequire } from 'node:module'; // These two lines allow us to use ES methods and CJS methods for loading dependencies.\
+import { rps, rpsls } from "./lib/rpsls.js";
 const require = createRequire(import.meta.url);
-// The above two lines allow us to use ES methods and CJS methods for loading
-// dependencies.
-// Load minimist for command line argument parsing
-// https://www.npmjs.com/package/minimist
-const minimist = require('minimist')
-// Parse our command line arguments
-const args = minimist(process.argv.slice(2))
-// Are we debugging or testing?
-// If so, then let's look at our command line arguments just to see what is in there
-if (args.debug) {
+
+const minimist = require('minimist') // Load minimist for command line argument parsing https://www.npmjs.com/package/minimist
+
+const args = minimist(process.argv.slice(2)) // Parse our command line arguments
+
+if (args.debug) { // Are we debugging or testing? If so, then let's look at our command line arguments just to see what is in there
     console.info('Minimist parsed and created the following `args` object:')
     console.info(args)
 }
-// Did we call for help? 
-if (args.h || args.help) {
+
+
+if (args.h || args.help) { // Did we call for help? 
     console.log(`
 usage: node server.js --port=5000
 
@@ -37,68 +35,99 @@ It also creates logs in a common log format (CLF) so that you can better.
     `)
     process.exit(0)
 } 
-// Load express and other dependencies for serving HTML, CSS, and JS files
-import express from 'express'
-// Use CJS __filename and __dirname in ES module scope
-// https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
-import path from 'path'
+
+import express from 'express' // Load express and other dependencies for serving HTML, CSS, and JS files
+
+import path from 'path' // Use CJS __filename and __dirname in ES module scope https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
 import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-// Load dependencies for logging
-const fs = require('fs')
+
+const fs = require('fs') // Load dependencies for logging
 const morgan = require('morgan')
-// Create log path
-const logpath = args.log || args.l || process.env.LOGPATH || path.join(__dirname, 'log')
-if (!fs.existsSync(logpath)){
-    fs.mkdirSync(logpath);
-}
+
+const logpath = args.log || args.l || process.env.LOGPATH || path.join(__dirname, 'log') // Create log path
+if (!fs.existsSync(logpath)){ fs.mkdirSync(logpath) }
 if (args.debug) {
     console.info('HTTP server is logging to this directory:')
     console.info(logpath)
 }
-// Create an app server
-const app = express()
-// Set a port for the server to listen on
-const port = args.port || args.p || process.env.PORT || 8080
+
+const app = express() // Create an app server
+
+const port = args.port || args.p || process.env.PORT || 8080 // Set a port for the server to listen on
 // Load app middleware here to serve routes, accept data requests, etc.
-//
-// Create and update access log
-// The morgan format below is the Apache Foundation combined format but with ISO8601 dates
-app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
+
+app.use(morgan( // Create and update access log. The morgan format below is the Apache Foundation combined format but with ISO8601 dates
+    ':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
     {stream: fs.createWriteStream(path.join(logpath, 'access.log')), flags: 'a' }
 ))
-// Serve static files
-const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
+
+const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public') // Serve static files
 app.use('/', express.static(staticpath))
-// Create app listener
-const server = app.listen(port)
-// Create a log entry on start
-let startlog = new Date().toISOString() + ' HTTP server started on port ' + port + '\n'
-// Debug echo start log entry to STDOUT
-if (args.debug) {
-    console.info(startlog)
-} 
-// Log server start to file
-fs.appendFileSync(path.join(logpath, 'server.log'), startlog)
-// Exit gracefully and log
-process.on('SIGINT', () => {
-// Create a log entry on SIGINT
-    let stoppinglog =  new Date().toISOString() + ' SIGINT signal received: stopping HTTP server\n'
-//  Log SIGINT to file
-    fs.appendFileSync(path.join(logpath, 'server.log'), stoppinglog)
-// Debug echo SIGINT log entry to STDOUT
-    if (args.debug) {
-        console.info('\n' + stoppinglog)
-    }
-// Create a log entry on stop
-    server.close(() => {
+
+
+app.get('/app', (req, res) => { // App get requests
+    res.status(200).send('200 OK');
+})
+
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(rps());
+})
+
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(rpsls());
+})
+
+app.get('/app/rps/play', (req, res) => {
+    res.status(200).send(rps(req.query.shot));
+})
+
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.query.shot));
+})
+
+app.get('/app/rps/play/:arg', (req, res) => {
+    res.status(200).send(rps(req.params.arg));
+})
+
+app.get('/app/rpsls/play/:arg', (req, res) => {
+    res.status(200).send(rpsls(req.params.arg));
+})
+
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(rps(req.body.shot));
+})
+
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.body.shot));
+})
+
+app.get('*', function(req, res){
+    res.status(404).send('404 NOT FOUND');
+  });
+
+const server = app.listen(port) // Create app listener
+
+let startlog = new Date().toISOString() + ' HTTP server started on port ' + port + '\n' // Create a log entry on start
+
+if (args.debug) { console.info(startlog) } // Debug echo start log entry to STDOUT 
+
+fs.appendFileSync(path.join(logpath, 'server.log'), startlog) // Log server start to file
+
+process.on('SIGINT', () => { // Exit gracefully and log
+
+    let stoppinglog =  new Date().toISOString() + ' SIGINT signal received: stopping HTTP server\n' // Create a log entry on SIGINT
+
+    fs.appendFileSync(path.join(logpath, 'server.log'), stoppinglog) //  Log SIGINT to file
+
+    if (args.debug) { console.info('\n' + stoppinglog) } // Debug echo SIGINT log entry to STDOUT
+
+    server.close(() => { // Create a log entry on stop
         let stoppedlog = new Date().toISOString() + ' HTTP server stopped\n'
-// Log server stop to file
-        fs.appendFileSync(path.join(logpath, 'server.log'), stoppedlog)
-// Debug echo stop log entry to STDOUT
-        if (args.debug) {
-            console.info('\n' + stoppedlog)
-        }    
+
+        fs.appendFileSync(path.join(logpath, 'server.log'), stoppedlog) // Log server stop to file
+
+        if (args.debug) { console.info('\n' + stoppedlog) } // Debug echo stop log entry to STDOUT
     })
 })
